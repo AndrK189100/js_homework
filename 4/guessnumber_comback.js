82c684b80c3aac3b10b2e035575d37f1a2a9d540
AndrK189100 <andrka@gmail.com>
2022-11-16 18:58:00 +0300

    taks 4.3 corrected

--- 4/guessnumber_comback.js
+++ 4/guessnumber_comback.js
@@ -1,11 +1,11 @@
-
 const rl = require('readline').createInterface(process.stdin, process.stdout);
 
 const questionPromise = function(query) {
     return new Promise ((resolve) => {rl.question(query, (data) => {resolve(data);})})
 }
 
-async function gameStart() {
+
+async function game() {
     let count = 1;
     let guessNumber = 0;
     while(true) {
@@ -25,54 +25,47 @@ async function gameStart() {
                 continue;
             }
         }
-
         rl.write('Угадайте число от [1; 10]\n');
         guessNumber = Math.floor(Math.random() * 10) + 1;
         console.log('Число: ', guessNumber);
-        game();
+        while(true) {
+            rl.write(`Поытка: ${count}\n`);
+            const answer = await questionPromise('Введите целое число от 1 до 10 или q для выхода: ');
+        
+            if(!Number.isInteger(Number(answer))) {
+                if(answer.toLowerCase() === 'q') {
+                    rl.close(); 
+                    console.clear();
+                    return;
+                }
+                rl.write('Неверный ввод\n ');
+                continue;
+            }
+            if(Number(answer) > 10 || Number(answer) < 1) {
+                rl.write('Неверный ввод\n ');
+                continue;
+            }
+            if(Number(answer) < guessNumber) {
+                rl.write('Введенное число меньше\n');
+                count++;
+                continue;
+            }
+            else if(Number(answer) > guessNumber) {
+                rl.write('Введенное число больше\n');
+                count++;
+                continue;
+            }
+            else {
+                rl.write(`Число угадано. Количество попыток: ${count}\n`);
+                break;
+            }
+            
+        }
+        await questionPromise('Нажми ввод');
+        console.clear();
+
     }
 
-    async function game() {
-        rl.write(`Поытка: ${count}\n`);
-        const answer = await questionPromise('Введите целое число от 1 до 10 или q для выхода: ');
-        
-        if(!Number.isInteger(Number(answer))) {
-            if(answer.toLowerCase() === 'q') {
-                rl.close(); 
-                console.clear();
-                return;
-            }
-            rl.write('Неверный ввод\n ');
-            game();
-            return;
-        }
-
-        if(Number(answer) > 10 || Number(answer) < 1) {
-            rl.write('Неверный ввод\n ');
-            game();
-            return;
-        }
-
-        if(Number(answer) < guessNumber) {
-            rl.write('Введенное число меньше\n');
-            count++;
-            game();
-            return;
-        }
-        else if(Number(answer) > guessNumber) {
-            rl.write('Введенное число больше\n');
-            count++;
-            game();
-            return;
-        }
-        else {
-            rl.write(`Число угадано. Количество попыток: ${count}\n`);
-            await questionPromise('Нажми ввод');
-            gameStart();
-            return;
-        }
-    }    
 }
 
-
-gameStart();
+game();
